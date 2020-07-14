21db4811a3d18d0248fbffa78b7a530e641c3304
Joseph cohen <jojocohen57@gmail.com>
2020-07-14 17:52:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,27 @@
-class Dog 
-  attr_accessor :name 
-  
- @@all = []
- 
+class Dog
+  attr_accessor :name
+
+  @@all = []
+
   def initialize(name)
-    @name = name   
+    @name = name
     save
-  end 
-  
-  def self.all 
+  end
+
+  def self.all
     @@all
-  end   
-  
-   def print_all 
-    @@all.each do |name|
-      puts name
-   end
-  
-   def clear_all
+  end
+
+  def self.print_all
+    # puts @@all.map(&:name) - another way to solve, but more complex than needed for this example
+    puts @@all.map{ |dog| dog.name }
+  end
+
+  def self.clear_all
     @@all.clear
-   end
-   
-   def save
+  end
+
+  def save
     @@all << self
-   end   
-  
-end   
+  end
+end
